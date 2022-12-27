d94ee2327876c710d04897cfbcb2c4010c95e8f9
Hiroki TAKEMURA <kekeho@sfc.wide.ad.jp>
2022-12-27 11:20:58 +0900

    update: config for sui 0.19.0

--- networks/sui/run.sh
+++ networks/sui/run.sh
@@ -1,6 +1,7 @@
 sui start --network.config /root/.sui/sui_config/network.yaml &
 sleep 3
-sui client transfer --to 0x08f9a17ca2375b9e9a7df5348ce4f3f59534388d --object-id 0x0601e7fb4a37f0c312cc4c23bfd9332a367c269d --gas-budget 1000
-sui client transfer --to 0xaa37732d5069a2153a2fcbd1118990d32ac3c176 --object-id 0x175c5fe4d123d2681da176d7127db47082f5e084 --gas-budget 1000
+sui client switch --address 0xfef3945b4fc518b416377bdce6cc0ded70a013ad
+sui client transfer --to 0x08f9a17ca2375b9e9a7df5348ce4f3f59534388d --object-id 0x2420ca76d9a43a85e835d3da89e3e83cb703aaba --gas-budget 1000
+sui client transfer --to 0xaa37732d5069a2153a2fcbd1118990d32ac3c176 --object-id 0x467eba8ecffe8354db8a740163b5e3dd59f52570 --gas-budget 1000
 
 sleep infinity
